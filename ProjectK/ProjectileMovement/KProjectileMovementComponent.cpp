948406baf34f36c6677028ffcaed091d3ebf618e
Lee Jae Hwan <45430116+ManHwany@users.noreply.github.com>
2023-12-09 01:54:14 +0900

    Projectile Movement
    발사체 움직임 구현

---
+++ ProjectK/ProjectileMovement/KProjectileMovementComponent.cpp
@@ -0,0 +1,464 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+#include "KProjectileMovementComponent.h"
+
+#include "Holder/KMvcHolder.h"
+#include "GameModule/KAte/Interface/IKAteMv.h"
+#include "GameModule/KAte/Mvc/ModelView/Animation/KAteAnimDef.h"
+#include "GameModule/KAte/Mvc/ModelView/Helper/KAteMissileLauncher.h"
+#include "GameModule/KAte/Mvc/ModelView/Actor/Missile/KAteMissile.h"
+
+#include "Components/MeshComponent.h"
+#include "Curves/CurveFloat.h"
+
+#include "Core/KLog.h"
+#include "Core/KMath.h"
+
+#include "Engine/Public/DrawDebugHelpers.h"
+
+UKProjectileMovementComponent::UKProjectileMovementComponent()
+{
+	PrimaryComponentTick.bCanEverTick = false;
+}
+
+#if WITH_EDITOR
+void UKProjectileMovementComponent::PostEditChangeProperty(FPropertyChangedEvent& PropertyChangedEvent)
+{
+	Super::PostEditChangeProperty(PropertyChangedEvent);
+
+	FName propertyName = PropertyChangedEvent.GetPropertyName();
+
+	if (propertyName.IsEqual("bUseBlendTime"))
+	{
+		bUseMovementSpeed = !bUseBlendTime;
+	}
+	else if (propertyName.IsEqual("bUseMovementSpeed"))
+	{
+		bUseBlendTime = !bUseMovementSpeed;
+	}
+}
+#endif	
+
+void UKProjectileMovementComponent::OnRegister()
+{
+	Super::OnRegister();
+	OwnerActor = GetOwner();
+}
+
+void UKProjectileMovementComponent::BeginPlay()
+{
+	Super::BeginPlay();
+
+	TotalCurveArea = calcTotalCurveArea(SpeedCurveFloat);
+}
+
+void UKProjectileMovementComponent::MoveToLocation(const FVector& location)
+{
+	DesiredLocation = location;
+
+	init();
+}
+
+void UKProjectileMovementComponent::MoveToActor(AActor* targetActor, FName socketName /**= "Root" */, const FVector locationOffset /** = FVector::ZeroVector*/)
+{
+	if (!targetActor)
+		return;
+
+	TargetActor = targetActor;
+	SocketName = socketName;
+	OffsetLocation = locationOffset;
+	DesiredLocation = targetActor->GetActorLocation();
+	TargetMeshComp = findTargetSkeletalMesh(targetActor);
+
+	init();
+	setTargetSocketInfo();
+}
+
+void UKProjectileMovementComponent::MoveToComponent(USceneComponent * targetComponent, FName socketName /**= NAME_None */, const FVector locationOffset /** = FVector::ZeroVector*/)
+{
+	if (!targetComponent)
+		return;
+
+	TargetComponent = targetComponent;
+	SocketName = socketName;
+	OffsetLocation = locationOffset;
+	DesiredLocation = TargetComponent->GetComponentLocation();
+	TargetMeshComp = findTargetSkeletalMesh(targetComponent);
+
+	init();
+	setTargetSocketInfo();
+}
+
+void UKProjectileMovementComponent::init()
+{
+	OwnerActor = GetOwner();
+	if (!OwnerActor.IsValid())
+	{
+		return;
+	}
+
+	StartLocation = OwnerActor->GetActorLocation();
+
+	BlendTime = bUseRandomBlendTime ? FMath::RandRange(BlendTimeMinMax.X, BlendTimeMinMax.Y) : BlendTime;
+	MovementSpeed = bUseRandomSpeed ? FMath::RandRange(MovementSpeedMinMax.X, MovementSpeedMinMax.Y) : MovementSpeed;
+	Amplitude = bUseRandomAmplitude ? FMath::RandRange(AmplitudeMinMax.X, AmplitudeMinMax.Y) : Amplitude;
+	FireAngle = bUseRandomFireAngle ? FMath::RandRange(FireAngleMinMax.X, FireAngleMinMax.Y) : FireAngle;
+
+	BlendTimeToGo = BlendTime;
+	bStartMove = true;
+
+	bCasterOnLeftFromTarget = IsCasterOnLeftFromDesiredLocation();
+}
+
+void UKProjectileMovementComponent::MovementTick(float deltaTime)
+{
+	if (!bStartMove)
+	{
+		return;
+	}
+
+	if (!OwnerActor.IsValid() || OwnerActor->IsPendingKill())
+	{
+		return;
+	}
+
+	const float prevElapsedTime = ElapsedTime;
+	ElapsedTime += deltaTime;
+
+	//도착위치 계산.
+	DesiredLocation = calcDesireLocation();
+
+	// 진행방향이 곧 forward 임.
+	const FVector forwardDir = (DesiredLocation - StartLocation).GetSafeNormal();
+
+	// 파형을 추가할 방향이다. forward 축을 기반으로, 설정된 발사각으로 파형을 만들어야함.
+	const FVector waveformDir = FVector::UpVector.RotateAngleAxis(FireAngle, forwardDir);
+
+	// 매틱 블렌딩 위치
+	FVector resultLocation = FVector::ZeroVector;
+
+	// ** 속도 기반 발세체 일 경우 **
+	if (bUseMovementSpeed)
+	{
+		// 시간 = 거리 / 속도
+		BlendTime = FVector::Dist(DesiredLocation, StartLocation) / MovementSpeed;
+
+		if (bUseSpeedCurve  && SpeedCurveFloat)
+		{
+			if (TotalCurveArea > 0.f)
+			{
+				BlendTime = BlendTime * (1.f / TotalCurveArea);
+			}
+		}
+
+		// 경과시간이 예측된 블렌딩 시간보다 커질경우 도착했다고 판단.
+		if (ElapsedTime >= BlendTime)
+		{
+			OwnerActor->SetActorLocation(DesiredLocation);
+			clear();
+			return;
+		}
+		else
+		{
+			const float alpha = ElapsedTime / BlendTime;
+
+			if (bUseSpeedCurve && SpeedCurveFloat)
+			{
+// 				const float prevAlpha = prevElapsedTime / BlendTime;
+// 				const float moveDistance = calcCurveArea(SpeedCurveFloat, prevAlpha, alpha, GetWorld()->GetDeltaSeconds());
+// 				// 위치 = 이전 위치 + 이번 틱에 이동한 거리
+// 				resultLocation = OwnerActor->GetActorLocation() + forwardDir * (moveDistance * MovementSpeed * BlendTime);
+
+				const float moveDistance = calcCurveArea(SpeedCurveFloat, 0.f, alpha, GetWorld()->GetDeltaSeconds());
+				// 위치 = 시작위치 + (시작위치 기준 - 커브 이동거리) * 속도 * 총 시간
+				resultLocation = StartLocation + forwardDir * (moveDistance * MovementSpeed * BlendTime);
+			}
+			else
+			{
+				// 위치 = 시작위치 + (속도 * 경과 시간)
+				resultLocation = StartLocation + (forwardDir * MovementSpeed* ElapsedTime);
+			}
+
+			// 포물선 커브가 존재 할 경우,
+			if (bUseParabolaMove && AmplitudeCurveFloat != nullptr)
+			{
+				const float moveMentOffset = AmplitudeCurveFloat->GetFloatValue(alpha) * Amplitude;
+				resultLocation += waveformDir * moveMentOffset;
+			}
+
+			// 가이드 포물선 무브먼트 적용
+			resultLocation += getAddedGuidedCurveMovement(alpha);
+		}
+	}
+	// ** 시간 기반 발세체 일 경우 **
+	else
+	{
+		BlendTimeToGo -= deltaTime;
+
+		if (BlendTimeToGo > 0.f)
+		{
+			const float blendPct = getBlendPct(BlendTimeToGo, deltaTime);
+			const float v = FVector::Dist(DesiredLocation, StartLocation) * blendPct / ElapsedTime;
+			const float alpha = (BlendTime - BlendTimeToGo) / BlendTime;
+
+			resultLocation = StartLocation + (forwardDir * (v * ElapsedTime));
+
+			// 포물선 커브가 존재 할 경우,
+			if (bUseParabolaMove && AmplitudeCurveFloat != nullptr)
+			{
+				const float moveMentOffset = AmplitudeCurveFloat->GetFloatValue(alpha) * Amplitude;
+				resultLocation += waveformDir * moveMentOffset;
+			}
+
+			// 가이드 포물선 무브먼트 적용
+			resultLocation += getAddedGuidedCurveMovement(alpha);
+		}
+		else
+		{
+			OwnerActor->SetActorLocation(DesiredLocation);
+			clear();
+			return;
+		}
+	}
+
+	// 발사체가 바라보는 방향은, 현재 위치에서 다음틱의 위치로 바라보면 됨.
+	const FRotator lookAtRot = FKMath::MakeLookAtRotation(OwnerActor->GetActorLocation(), resultLocation);
+
+	OwnerActor->SetActorRotation(lookAtRot);
+	OwnerActor->SetActorLocation(resultLocation);
+}
+
+const FVector UKProjectileMovementComponent::getAddedGuidedCurveMovement(const float alpha)
+{
+	if (bUseGuidedParabola && GuidedCurveFloat)
+	{
+		const float dotValueAbs = FMath::Abs(DotProductResult);
+
+		// [5/20/2020 jhlee01] 왼쪽 오른쪽 가릴필요없이, 거의 수평으로 서있다면 보정 하지말고.
+		if (dotValueAbs < 0.1f)
+		{
+			return FVector::ZeroVector;
+		}
+
+		// 왼쪽에 있으면, 옆으로 눕힐거니 90도 출발.
+		const float guidedFireAngle = 90.f;
+
+		const FVector forwardDir = (DesiredLocation - StartLocation).GetSafeNormal();
+
+		const FVector guidedformDir = FVector::UpVector.RotateAngleAxis(guidedFireAngle, forwardDir);
+
+		// 내적 값에 의존하여 휘는정도를 세팅한다.
+		const float moveMentOffset = GuidedCurveFloat->GetFloatValue(alpha) * DotProductResult * GuidedAmplitude;
+
+		return guidedformDir * moveMentOffset;
+	}
+
+	return FVector::ZeroVector;
+}
+
+USkeletalMeshComponent * UKProjectileMovementComponent::findTargetSkeletalMesh(AActor * targetActor)
+{
+	if (!IsValid(targetActor))
+	{
+		return nullptr;
+	}
+	USkeletalMeshComponent* mesh = Cast<USkeletalMeshComponent>(targetActor->GetComponentByClass(UMeshComponent::StaticClass()));
+	return mesh;
+}
+
+USkeletalMeshComponent * UKProjectileMovementComponent::findTargetSkeletalMesh(USceneComponent * targetComp)
+{
+	AActor* ownerActor = targetComp->GetOwner();
+	return findTargetSkeletalMesh(ownerActor);
+}
+
+float UKProjectileMovementComponent::getBlendPct(const float blendTimeToGo, const float deltaTime)
+{
+	float blendPct = 0.f;
+	float blendTime = BlendTime;
+	float DurationPct = (blendTime - blendTimeToGo) / blendTime;
+
+	if (bUseSpeedCurve && SpeedCurveFloat)
+	{
+		blendPct = SpeedCurveFloat->GetFloatValue(DurationPct);
+	}
+	else
+	{
+		switch (SpeedBlendFunction)
+		{
+			case EViewTargetBlendFunction::VTBlend_Linear:
+				blendPct = FMath::Lerp(0.f, 1.f, DurationPct);
+				break;
+			case EViewTargetBlendFunction::VTBlend_Cubic:
+				blendPct = FMath::CubicInterp(0.f, 0.f, 1.f, 0.f, DurationPct);
+				break;
+			case EViewTargetBlendFunction::VTBlend_EaseIn:
+				blendPct = FMath::Lerp(0.f, 1.f, FMath::Pow(DurationPct, SpeedBlendExp));
+				break;
+			case EViewTargetBlendFunction::VTBlend_EaseOut:
+				blendPct = FMath::Lerp(0.f, 1.f, FMath::Pow(DurationPct, 1.f / SpeedBlendExp));
+				break;
+			case EViewTargetBlendFunction::VTBlend_EaseInOut:
+				blendPct = FMath::InterpEaseInOut(0.f, 1.f, DurationPct, SpeedBlendExp);
+				break;
+			default:
+				break;
+		}
+	}
+
+	return blendPct;
+}
+
+void UKProjectileMovementComponent::clear()
+{
+	bStartMove = false;
+	OnCompletedMovement.ExecuteIfBound(DesiredLocation);
+}
+
+void UKProjectileMovementComponent::SetCompletedCallback(FKOnCompletedMovement onCompleted)
+{
+	OnCompletedMovement = onCompleted;
+}
+
+const FVector UKProjectileMovementComponent::getSocketLocation(TWeakObjectPtr<AActor> actor, FName socketName)
+{
+	FVector resultLocation = FVector::ZeroVector;
+
+	if (actor != nullptr)
+	{
+		if (UMeshComponent* mesh = Cast<UMeshComponent>(actor->GetComponentByClass(UMeshComponent::StaticClass())))
+		{
+			resultLocation = mesh->GetSocketLocation(socketName);
+		}
+	}
+
+	return resultLocation;
+}
+
+FVector UKProjectileMovementComponent::getLandLocation(TWeakObjectPtr<USceneComponent> component)
+{
+	if (!component.IsValid())
+		return FVector::ZeroVector;
+
+	FHitResult hitResult;
+	const FVector componentLoc = component->GetComponentLocation();
+
+	//
+	if (component->GetWorld()->LineTraceSingleByChannel(hitResult, componentLoc + (FVector::UpVector * 1000.f), componentLoc - (FVector::UpVector * 1000.f), ECC_GameTraceChannel9))
+	{
+		return hitResult.ImpactPoint;
+	}
+
+	return component->GetComponentLocation();
+}
+
+bool UKProjectileMovementComponent::IsCasterOnLeftFromDesiredLocation()
+{
+	auto missile = Cast<AKAteMissile>(OwnerActor);
+
+	if (!missile)
+	{
+		return false;
+	}
+
+	auto caster = missile->GetCasterActor();
+
+	if (!caster.IsValid())
+	{
+		return false;
+	}
+
+	const FVector upVector = FVector::UpVector;
+	const FVector casterForward = caster->GetActorForwardVector();
+	const FVector toCasterDir = (caster->GetActorLocation() - DesiredLocation).GetSafeNormal();
+
+	DotProductResult = FVector::DotProduct(upVector, FVector::CrossProduct(toCasterDir, casterForward));
+	GuidedAmplitude = bUseRandomGuidedAmplitude ? FMath::RandRange(GuidedAmplitudeMinMax.X, GuidedAmplitudeMinMax.Y) : GuidedAmplitude;
+
+	return DotProductResult < 0 ? false : true;
+}
+
+void UKProjectileMovementComponent::setTargetSocketInfo()
+{
+	bRootSocket = SocketName.IsEqual(KAteAgentSocketName::Root);
+	if(bRootSocket)
+	{
+		//타겟 소켓의 이름이 Root일때 RootOverride 소켓이있다면 해당 소켓으로 타겟을 덮어 씌운다.
+		if (TargetMeshComp.IsValid())
+		{
+			bRootOverrideSocket = TargetMeshComp->DoesSocketExist(KAteAgentSocketName::Root_Override);
+			if (bRootOverrideSocket)
+			{
+				SocketName = KAteAgentSocketName::Root_Override;
+			}
+		}
+	}
+}
+
+
+float UKProjectileMovementComponent::calcTotalCurveArea(UCurveFloat* curveFloat)
+{
+	if (curveFloat == nullptr)
+		return 0.f;
+
+	float minTime = 0.f;
+	float maxTime = 0.f;
+
+	curveFloat->GetTimeRange(minTime, maxTime);
+
+	return calcCurveArea(curveFloat, minTime, maxTime, GetWorld()->GetDeltaSeconds());
+}
+
+float UKProjectileMovementComponent::calcCurveArea(UCurveFloat* curveFloat, float minTime, float maxTime, float deltaTime)
+{
+	if (curveFloat == nullptr)
+		return 0.f;
+
+	if (deltaTime <= 0.f)
+		return 0.f;
+
+	float area = 0.f;
+
+	for (float time = minTime; time < maxTime; time += deltaTime)
+	{
+		const float nextTime = FMath::Min<float>(time + deltaTime, maxTime);
+		const float curValue = curveFloat->GetFloatValue(time);
+
+		area += curValue * (nextTime - time);
+	}
+
+	return area;
+}
+
+FVector UKProjectileMovementComponent::calcDesireLocation()
+{
+	FVector desireLoc = FVector::ZeroVector;
+	if (TargetActor.IsValid() && !TargetActor->IsPendingKill())
+	{
+		// 타겟 액터가 존재하면, 목표위치는 타겟액터의 특정 소켓 부위 + Offset 이다.
+		desireLoc = FKAteMissileLauncher::GetLocalTransform(TargetActor.Get(), SocketName, OffsetLocation).GetLocation();
+	}
+	else if (TargetComponent.IsValid() && !TargetComponent->IsPendingKill())
+	{
+		//타겟 컴포넌트가 존재할때.
+		if (bRootOverrideSocket)
+		{
+			if (TargetMeshComp.IsValid())
+			{
+				const FTransform rootTr = TargetMeshComp->GetSocketTransform(KAteAgentSocketName::Root_Override); 
+				FVector compLoc = TargetComponent->GetComponentTransform().GetLocation();
+				compLoc.Z = rootTr.GetLocation().Z;
+				desireLoc = compLoc + FKAteMissileLauncher::ChangeToLocalVector(TargetComponent.Get(), OffsetLocation);
+			}
+		}
+		else if (bRootSocket)
+		{
+			//만약 소켓위치가 Root라면 바닥에 출력한다.
+			desireLoc = getLandLocation(TargetComponent) + FKAteMissileLauncher::ChangeToLocalVector(TargetComponent.Get(), OffsetLocation);
+		}
+		else
+		{
+			desireLoc = TargetComponent->GetComponentTransform().GetLocation() + FKAteMissileLauncher::ChangeToLocalVector(TargetComponent.Get(), OffsetLocation);
+		}
+	}
+	return desireLoc;
+}
---
+++ ProjectK/ProjectileMovement/KProjectileMovementComponent.h
@@ -0,0 +1,221 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Camera/PlayerCameraManager.h"
+#include "Components/SceneComponent.h"
+#include "KProjectileMovementComponent.generated.h"
+
+DECLARE_DELEGATE_OneParam(FKOnCompletedMovement, const FVector&);
+
+class UCurveFloat;
+class USkeletalMeshComponent;
+
+UCLASS(ClassGroup = (Custom), meta = (BlueprintSpawnableComponent), hidecategories = (Transform, Mobility, Socket, Rendering, Tags, ComponentReplication, Activation, Cooking, Physics, LOD, AssetUserData, Collision, "Components|Activation"))
+class KGAME_API UKProjectileMovementComponent : public USceneComponent
+{
+	GENERATED_BODY()
+
+private:
+	FKOnCompletedMovement OnCompletedMovement;
+
+	FVector StartLocation = FVector::ZeroVector;
+	FVector DesiredLocation = FVector::ZeroVector;
+
+	bool bStartMove = false;
+
+	float BlendTimeToGo = 0.f;
+	float ElapsedTime = 0.f;
+
+	// source 액터
+	TWeakObjectPtr<AActor> OwnerActor;
+
+	// 타겟 액터
+	TWeakObjectPtr<AActor> TargetActor;
+
+	// 타겟 컴포넌트
+	TWeakObjectPtr<USceneComponent> TargetComponent;
+
+	//타겟 액터와 컴포넌트의 타겟이 되는 메쉬 컴포넌트.
+	TWeakObjectPtr<USkeletalMeshComponent> TargetMeshComp;
+
+	// 타겟 액터의 소켓이름
+	FName SocketName = NAME_None;
+
+	bool bRootSocket = false;
+
+	bool bRootOverrideSocket = false;
+
+	// 종료 지점에 가중될 오프셋 위치
+	FVector OffsetLocation = FVector::ZeroVector;
+
+	// 캐스터가 타겟보다 왼쪽에 있는지 여부.
+	bool bCasterOnLeftFromTarget = false;
+
+	// 왼쪽 판별 Value. ( 내적 값 )
+	float DotProductResult = 0.f;
+
+private:
+	/** 블렌딩 시간을 이용하여 날릴지에 대한 여부 */
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, meta = (Category = "KProjectile Move by Time Blend", AllowPrivateAccess = true))
+	bool bUseBlendTime = false;
+
+	/** 랜덤 블렌딩 시간 사용 여부 */
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, meta = (Category = "KProjectile Move by Time Blend", AllowPrivateAccess = true, editcondition = "!bUseMovementSpeed && bUseBlendTime"))
+	bool bUseRandomBlendTime = false;
+
+	/** 랜덤 블렌딩 시간 범위 */
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, meta = (Category = "KProjectile Move by Time Blend", AllowPrivateAccess = true, editcondition = "!bUseMovementSpeed && bUseBlendTime && bUseRandomBlendTime"))
+	FVector2D BlendTimeMinMax = FVector2D::ZeroVector;
+
+	/** 블렌딩 시간 */
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, meta = (Category = "KProjectile Move by Time Blend", AllowPrivateAccess = true, editcondition = "!bUseMovementSpeed && bUseBlendTime && !bUseRandomBlendTime"))
+	float BlendTime = 0.f;
+
+	/** 블렌딩 Function */
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, meta = (Category = "KProjectile Move by Time Blend", AllowPrivateAccess = true, editcondition = "!bUseCustomCurve &&bUseBlendTime && !bUseMovementSpeed"))
+	TEnumAsByte<enum EViewTargetBlendFunction> SpeedBlendFunction;
+
+	/** 블렌딩 가중치 */
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, meta = (Category = "KProjectile Move by Time Blend", AllowPrivateAccess = true, editcondition = "!bUseCustomCurve && bUseBlendTime && !bUseMovementSpeed"))
+	float SpeedBlendExp = 0.f;
+
+	/************************************************************************/
+
+	/** 무브 스피드를 이용하여 날릴지에 대한 여부 */
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, meta = (Category = "KProjectile Move by Speed", AllowPrivateAccess = true))
+	bool bUseMovementSpeed = true;
+
+	/** 랜덤 스피드 사용 여부 */
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, meta = (Category = "KProjectile Move by Speed", AllowPrivateAccess = true, editcondition = "!bUseBlendTime && bUseMovementSpeed"))
+	bool bUseRandomSpeed = false;
+
+	/** 랜덤 스피드 Min Max 범위 */
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, meta = (Category = "KProjectile Move by Speed", AllowPrivateAccess = true, editcondition = "!bUseBlendTime && bUseMovementSpeed && bUseRandomSpeed"))
+	FVector2D MovementSpeedMinMax = FVector2D::ZeroVector;
+
+	/** 무브 스피드 */
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, meta = (Category = "KProjectile Move by Speed", AllowPrivateAccess = true, editcondition = "!bUseBlendTime && bUseMovementSpeed && !bUseRandomSpeed"))
+	float MovementSpeed = 1600.f;
+
+	/************************************************************************/
+
+	/** 포물선 움직임 사용 여부 */
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, meta = (Category = "KProjectile ParabolaMove", AllowPrivateAccess = true))
+	bool bUseParabolaMove = false;
+
+	/** 포물선 움직임 커브 */
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, meta = (Category = "KProjectile ParabolaMove", AllowPrivateAccess = true, editcondition = "bUseParabolaMove"))
+	UCurveFloat* AmplitudeCurveFloat = nullptr;
+
+	/** 포물선 랜덤 진폭 사용 여부 */
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, meta = (Category = "KProjectile ParabolaMove", AllowPrivateAccess = true, editcondition = "bUseParabolaMove"))
+	bool bUseRandomAmplitude = false;
+
+	/** 랜덤 진폭 범위 */
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, meta = (Category = "KProjectile ParabolaMove", AllowPrivateAccess = true, editcondition = "bUseParabolaMove && bUseRandomAmplitude"))
+	FVector2D AmplitudeMinMax = FVector2D::ZeroVector;
+
+	/** 고정 진폭 값 */
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, meta = (Category = "KProjectile ParabolaMove", AllowPrivateAccess = true, editcondition = "bUseParabolaMove && !bUseRandomAmplitude"))
+	float Amplitude = 1.f;
+
+	/** 유도 포물선 움직임 사용 여부 */
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, meta = (Category = "KProjectile Guided ParabolaMove", AllowPrivateAccess = true))
+	bool bUseGuidedParabola = false;
+
+	/** 유도 포물선 */
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, meta = (Category = "KProjectile Guided ParabolaMove", AllowPrivateAccess = true, editcondition = "bUseGuidedParabola"))
+	UCurveFloat* GuidedCurveFloat = nullptr;
+
+	/** 유도 포물선 랜덤 진폭 사용 여부 */
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, meta = (Category = "KProjectile Guided ParabolaMove", AllowPrivateAccess = true, editcondition = "bUseGuidedParabola"))
+	bool bUseRandomGuidedAmplitude = false;
+
+	/** 유도 포물선 랜덤 진폭 범위 */
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, meta = (Category = "KProjectile Guided ParabolaMove", AllowPrivateAccess = true, editcondition = "bUseGuidedParabola && bUseRandomGuidedAmplitude"))
+	FVector2D GuidedAmplitudeMinMax = FVector2D::ZeroVector;
+
+	/** 유도 포물선 고정 진폭 값 */
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, meta = (Category = "KProjectile Guided ParabolaMove", AllowPrivateAccess = true, editcondition = "bUseGuidedParabola && !bUseRandomGuidedAmplitude"))
+	float GuidedAmplitude = 1.f;
+
+	/************************************************************************/
+
+	/** 랜덤 발사 각도 사용 여부 */
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, meta = (Category = "KProjectile Fire Option", AllowPrivateAccess = true))
+	bool bUseRandomFireAngle = false;
+
+	/** 랜덤 발사 각도 범위 */
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, meta = (Category = "KProjectile Fire Option", AllowPrivateAccess = true, editcondition = "bUseRandomFireAngle"))
+	FVector2D FireAngleMinMax = FVector2D::ZeroVector;
+
+	/** 발사각 */
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, meta = (Category = "KProjectile Fire Option", AllowPrivateAccess = true, editcondition = "!bUseRandomFireAngle"))
+	float FireAngle = 0.f;
+		
+	/************************************************************************/
+
+	/** 커스텀 스피드 커브 사용 여부 */
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, meta = (Category = "KProjectile Speed Curve", AllowPrivateAccess = true))
+	bool bUseSpeedCurve = false;
+
+	/** 커스텀 스피드 커브 ( For move speed ) */
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, meta = (Category = "KProjectile Speed Curve", AllowPrivateAccess = true, editcondition = "bUseSpeedCurve"))
+	UCurveFloat* SpeedCurveFloat = nullptr;
+
+	float TotalCurveArea = 0.f;
+
+	//UPROPERTY(EditAnywhere, BlueprintReadOnly, meta = (Category = "KProjectile Fire Option", AllowPrivateAccess = true))
+	//float AreaDeltaTime = 0.01f;
+
+protected:
+	UKProjectileMovementComponent();
+	virtual void OnRegister() override;
+	virtual void BeginPlay() override;
+
+#if WITH_EDITOR
+	virtual void PostEditChangeProperty(FPropertyChangedEvent& PropertyChangedEvent) override;
+#endif	
+
+private:
+	void init();
+	void clear();
+	float getBlendPct(const float blendTimeToGo, const float deltaTime);
+	const FVector getSocketLocation(TWeakObjectPtr<AActor> actor, FName socketName);
+	FVector getLandLocation(TWeakObjectPtr<USceneComponent> component);
+	bool IsCasterOnLeftFromDesiredLocation();
+
+	// 소켓에 관한 데이터 셋팅.
+	void setTargetSocketInfo();
+
+	// 커브 영역 계산
+	float calcTotalCurveArea(UCurveFloat* curveFloat);
+
+	// 커브 영역 계산
+	float calcCurveArea(UCurveFloat* curveFloat, float minTime, float maxTime, float deltaTime);
+
+	FVector calcDesireLocation();
+
+	// 유도 커브 무브먼트 얻어오기
+	const FVector getAddedGuidedCurveMovement(const float alpha);
+
+	USkeletalMeshComponent* findTargetSkeletalMesh(AActor* targetActor);
+
+	USkeletalMeshComponent* findTargetSkeletalMesh(USceneComponent* targetComp);
+
+public:
+	UFUNCTION(BlueprintCallable)
+	void MoveToLocation(const FVector& location);
+
+	UFUNCTION(BlueprintCallable)
+	void MoveToActor(AActor* targetActor, FName socketName = "Root", const FVector locationOffset = FVector::ZeroVector);
+
+	UFUNCTION(BlueprintCallable)
+	void MoveToComponent(USceneComponent* targetComponent, FName socketName = NAME_None, const FVector locationOffset = FVector::ZeroVector);
+
+	void MovementTick(float deltaTime);
+
+	void SetCompletedCallback(FKOnCompletedMovement onCompleted = nullptr);
+};
